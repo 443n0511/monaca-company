a94bcbaf5f08d0a5b2149ccdf53b7a44f047f58d
sample <>
2019-12-18 01:34:04 +0900

    stickyの練習

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,3 +2,39 @@
 $(document).ready(function () {
     $('.drawer').drawer();
   });
+
+  $(function () {
+
+    /*
+     * Sticky header
+     */
+    $('.p-header').each(function () {
+
+        var $window = $(window), // ウィンドウを jQuery オブジェクト化
+            $header = $(this),   // ヘッダーを jQuery オブジェクト化
+            // ヘッダーのデフォルト位置を取得
+            headerOffsetTop = $header.offset().top;
+
+        // ウィンドウのスクロールイベントを監視
+        // (ウィンドウがスクロールするごとに処理を実行する)
+        $window.on('scroll', function () {
+            // ウィンドウのスクロール量をチェックし、
+            // ヘッダーのデフォルト位置を過ぎていればクラスを付与、
+            // そうでなければ削除
+            if ($window.scrollTop() > headerOffsetTop) {
+                $header.addClass('sticky');
+                $('.p-greeting--box').css('padding-top','100px');
+
+            } else {
+                $header.removeClass('sticky');
+                $('.p-greeting--box').css('padding-top','0');
+            }
+        });
+
+        // ウィンドウのスクロールイベントを発生させる
+        // (ヘッダーの初期位置を調整するため)
+        $window.trigger('scroll');
+
+    });
+});
+
